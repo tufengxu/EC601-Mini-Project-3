3784ba985e23b58b70753fd653cc5cb9c7a6dc57
tufengxu <55207104+tufengxu@users.noreply.github.com>
2019-12-08 22:38:22 -0500

    Add files via upload

--- DSP.py
+++ DSP.py
@@ -16,30 +16,27 @@ def visualization(filename):
   x, sr = librosa.load(filename)
   X = librosa.stft(x)
   Xdb = librosa.amplitude_to_db(abs(X))
+  for item in enumerate(WAVE.getparams()):
+      print(item)
+  a = WAVE.getparams().nframes
+  f = WAVE.getparams().framerate
+  sample_time = 1 / f
+  time = a / f
+  sample_frequency, audio_sequence = wavfile.read(filename)
+  print(audio_sequence)
+  x_seq = np.arange(0, time, sample_time)
+  y_seq = abs(fft(audio_sequence))
+
+  plt.plot(x_seq, audio_sequence, 'blue')
+  plt.xlabel("time (s)")
+  plt.ylabel("amplitude")
+  plt.title("Time domain signal")
+
   plt.figure(figsize=(14, 5))
   librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz')
   plt.colorbar()
   plt.show()
-  # for item in enumerate(WAVE.getparams()):
-  #     print(item)
-  # a = WAVE.getparams().nframes
-  # f = WAVE.getparams().framerate
-  # sample_time = 1 / f
-  # time = a / f
-  # sample_frequency, audio_sequence = wavfile.read(filename)
-  # print(audio_sequence)
-  # x_seq = np.arange(0, time, sample_time)
-  # y_seq = abs(fft(audio_sequence))
-  # plt.subplot(221)
-  # plt.plot(x_seq, audio_sequence, 'blue')
-  # plt.xlabel("time (s)")
-  # plt.ylabel("amplitude")
-  # plt.title("Time domain signal")
-  # plt.subplot(222)
-  # plt.plot(y_seq, abs(audio_sequence), 'red')
-  # plt.xlabel("frequency (Hz)")
-  # plt.title("Frequency domain signal")
-  # plt.show()
+  
 
 if __name__ == '__main__':
   file_music = 'Canon.wav'
